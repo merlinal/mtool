9d18f3ea33fad309ae484032ecd982a52d9831cd
赵安林 <zhaoal@cnsuning.com>
2017-09-11 15:37:16 +0800

    调试

---
+++ src/main/java/com/merlin/tool/Utils.java
@@ -0,0 +1,12 @@
+package com.merlin.tool;
+
+/**
+ * Created by Administrator on 2017/9/11.
+ */
+
+public class Utils {
+
+    public static void print(){
+        System.out.println("sfsdfsdfsdf");
+    }
+}
